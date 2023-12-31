c0f59d27f843663486f764ab501c7bc3719fa7ba
XuanhaoZu <zu.xu@northeastern.edu>
2023-12-30 20:28:43 -0500

    file upload frontend and backend

--- cloud-doc-hub-backend/lambda_functions/upload_document/app.py
+++ cloud-doc-hub-backend/lambda_functions/upload_document/app.py
@@ -1,29 +1,35 @@
 import boto3
-import os
 import json
+import base64
 from botocore.exceptions import ClientError
 
+# 初始化S3客户端
+s3 = boto3.client('s3')
+
 def lambda_handler(event, context):
-    # 解析上传的文件信息
-    file_content = event['content']
-    file_name = event['filename']
-
-    # S3 客户端初始化
-    s3_client = boto3.client('s3')
-
     try:
-        # 将文件上传到S3
-        response = s3_client.put_object(
-            Bucket=os.environ['S3_BUCKET'],
-            Key=file_name,
-            Body=file_content
-        )
+        # 解析前端传送的JSON数据
+        body = json.loads(event['body'])
+
+        # 文件数据通常通过base64编码传输
+        file_content = base64.b64decode(body['file_content'])
+        file_name = body['file_name']
+        bucket_name = 'cloud-doc-hub-bucket'  # 指定的S3 bucket名称
+
+        # 上传文件到S3
+        s3.put_object(Bucket=bucket_name, Key=file_name, Body=file_content)
+
         return {
-            "statusCode": 200,
-            "body": json.dumps("File uploaded successfully.")
-        }
+            'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*', 
+            'Access-Control-Allow-Methods': 'POST,OPTIONS'
+        },
+        'body': json.dumps('uploaded!')
+    }
     except ClientError as e:
         return {
-            "statusCode": 500,
-            "body": json.dumps(f"Error uploading file: {str(e)}")
+            'statusCode': 500,
+            'body': json.dumps(f"Error uploading file: {str(e)}")
         }
--- cloud-doc-hub-frontend/src/components/UploadForm.js
+++ cloud-doc-hub-frontend/src/components/UploadForm.js
@@ -3,19 +3,66 @@ import React, { useState } from 'react';
 function UploadForm({ onAddDocument }) {
   const [file, setFile] = useState(null);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    // 这里应该有代码来处理文件上传
-    // 暂时我们只是将文件名添加到列表中
+  
     if (file) {
-      onAddDocument(file.name);
+      const base64 = await toBase64(file).catch(error => {
+        console.error('Error converting file to base64:', error);
+        return;
+      });
+  
+      if (!base64) {
+        // 如果文件没有成功转换为 base64，就不继续执行
+        console.error('File conversion to base64 failed');
+        return;
+      }
+  
+      // 构建请求体
+      const data = JSON.stringify({
+        file_name: file.name,
+        file_content: base64
+      });
+  
+      // 发送 POST 请求到您的 Lambda 函数的 API 网关 URL
+      fetch('https://rgdooz55o2.execute-api.us-east-2.amazonaws.com/dev/upload', {
+        method: 'POST',
+        body: data,
+        headers: {
+          'Content-Type': 'application/json'  
+        }
+      })
+      .then(response => {
+        if (!response.ok) {
+          // 如果响应状态码不是 2xx, 抛出错误
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        onAddDocument(file.name); // 文件上传后添加到列表
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+      
       setFile(null);
     }
   };
+  
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
   };
+
+  // 将文件转换为Base64格式的函数
+  const toBase64 = (file) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result.split(',')[1]);
+    reader.onerror = error => reject(error);
+  });
 
   return (
     <form onSubmit={handleSubmit}>
